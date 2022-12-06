966f7f5e89be27046e719beac5fe0fc0eb9336b7
LeoZotos <zotos.leo@gmail.com>
2022-12-06 18:38:25 +0100

    started making a baseline for a semantic similarity score based on word overlap

--- src/semanticVariation.py
+++ src/semanticVariation.py
@@ -1,31 +1,65 @@
-import os
-import multiprocessing as mp
-import numpy as np
-from tqdm import tqdm
-from textblob import TextBlob
 from newspaper import Article
-
+from textblob import TextBlob
+from wordfreq import zipf_frequency
+from utils import calcJaccardSimilarity, calcIntersection
 from config import settings
-from utils import pickRandomItems
 
-def calcSemanticVariationBetweenTwo(url1, url2):
-    similarityScore = 0
-    similarityScore = np.random.randint(0, 10)
+def calcSemanticVariationBetweenTwo(rareWordsMainArticle, url2):    
+    # Given the rare words of the main article and a URL to another article, calculate the similarity score between them.
+    # If the overlap is large, the score will be high (i.e. high similarity).
     
+    # First, extract the rare words from the second article
+    rareWordsSecondArticle = extractRareWords(url2)
+    #Then, calculate the Jacard similarity between the two sets of rare words
+    similarityScore = calcJaccardSimilarity(rareWordsMainArticle, rareWordsSecondArticle)
+    print("There are " + str(len(rareWordsMainArticle)) + " rare words in the main article.")
+    print("There are " + str(len(rareWordsSecondArticle)) + " rare words in the article with url: " + url2)
+    print("The overlap is: " + str(len(calcIntersection(rareWordsMainArticle, rareWordsSecondArticle))) + " words.")
+    print("The Jaccard Similarity is:" + str(similarityScore))
     return similarityScore
 
 
-def calcSemanticVariation(mainArticle, relatedArticles):
+def extractRareWords(articleURL, zipfFrequency = 6): # zipfRareness needs tuning
+    # Given a URL, extracts the words that are rare in the English language, using the zipf distribution.
+    try:
+        articleText = Article(articleURL)
+        articleText.download()
+        articleText.parse()
+    except Exception as e:
+        if settings['debug'] == True:
+            print("Exception while downloading article: " + str(e))
+        return []
     
-    # for each related article, calculate the similarity score
+    articleText = articleText.text
+    blob = TextBlob(articleText)
+    articleNounPhrases = blob.noun_phrases
+    print("There are " + str(len(articleNounPhrases)) + " noun phrases in the article.")
+    articleNounPhrases = list(set(articleNounPhrases))
+    print("After removing duplicates, there are " + str(len(articleNounPhrases)) + " noun phrases in the article.")
+
+    rareWords = []
+    for word in articleNounPhrases:
+        if zipf_frequency(word, 'en') < zipfFrequency:
+            rareWords.append(word)
+
+    print("The shape of rareWords is: " + rareWords.shape())
+    print("---------START OF RARE WORDS---------")
+    print("The rare words are: " + str(rareWords))
+    print("---------END OF RARE WORDS---------")
+    return rareWords
+
+def calcSemanticVariation(mainArticle, relatedArticles):
+    # For each related article, calculate the similarity score to the main article.
+    
+    # Find the rare words in the main article here, so we only have to download it once
+    rareWordsMainArticle = extractRareWords(mainArticle)
+    
     relatedArticlesAndSimilarityScores = []
     for relatedArticle in relatedArticles:
-        similarityScore = calcSemanticVariationBetweenTwo(mainArticle, relatedArticle)
+        similarityScore = calcSemanticVariationBetweenTwo(rareWordsMainArticle, relatedArticle)
         relatedArticlesAndSimilarityScores.append([relatedArticle, similarityScore])
 
     # sort the list by similarity score
     relatedArticlesAndSimilarityScores.sort(key=lambda x: x[1], reverse=True)
     
     return relatedArticlesAndSimilarityScores
-    
-    
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,18 @@
 import random
 
-#function that picks x random items from list
+# Function that picks x random items from list
 def pickRandomItems(list, x):
     return random.sample(list, x)
+
+# Function that returns the intersection of two lists
+def calcIntersection(lst1, lst2):
+    lst3 = [value for value in lst1 if value in lst2]
+    return lst3
+
+# Function that returns the jaccard similarity of two sets
+def calcJaccardSimilarity(lst1, lst2):
+    nominator = calcIntersection(lst1, lst2)
+    denominator = lst1 + lst2 # union of the two lists
+    jaccardSimilarity = len(nominator)/len(denominator)
+    
+    return round(jaccardSimilarity, 3)
