57d76da8171ddb270f1aac57def978b6bb6db6c4
LeoZotos <zotos.leo@gmail.com>
2023-01-08 01:30:54 +0100

    added option for use of TF-IDF to compare the content of the related articles

--- src/semanticVariation.py
+++ src/semanticVariation.py
@@ -2,9 +2,19 @@ from textblob import TextBlob
 from wordfreq import zipf_frequency
 import string
 import re
+import nltk
+from nltk.corpus import stopwords
+import string
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 from utils import calcJaccardSimilarity, calcIntersection
 from config import settings
 
+
+
+def computeSimilarity(a, b, vectorizer):
+  tfidf = vectorizer.fit_transform([a, b])
+  return ((tfidf * tfidf.T).toarray())[0,1]
 
 def calcSemanticVariationBetweenTwo(rareWordsMainArticle, url2):
     # Given the rare words of the main article and a URL to another article, calculate the similarity score between them.
@@ -57,17 +67,40 @@ def extractRareWords(articleContent, zipfFrequency=6):
 def calcSemanticVariation(mainArticleContent, relatedArticlesContent):
     # For each related article, calculate the similarity score to the main article.
 
-    # Find the rare words in the main article here, so we only have to find them once
-    rareWordsMainArticle = extractRareWords(mainArticleContent)
+    if settings['useOfTFIDF'] == True:
+        remPunctuationMap = dict((ord(char), None) for char in string.punctuation)
+        
+        def preprocess(text): # We define it here so that we can use the remPunctuationMap variable
+            return nltk.word_tokenize(text.lower().translate(remPunctuationMap))
+    
+        enStopwords = stopwords.words('english')
+        vectorizer = TfidfVectorizer(tokenizer=preprocess, stop_words=enStopwords)
+        
+        # Get text of main article
+        relatedArticlesAndSimilarityScores = []
+        mainArticleText = mainArticleContent.text
+        for relatedArticle in relatedArticlesContent:
+            similarityScore = computeSimilarity(mainArticleText, relatedArticle.text, vectorizer)
+            relatedArticlesAndSimilarityScores.append(
+                [relatedArticle.url, similarityScore])
+        # sort the list by decreasing similarity score
+        relatedArticlesAndSimilarityScores.sort(key=lambda x: x[1],
+                                                reverse=False)
+        
 
-    relatedArticlesAndSimilarityScores = []
-    for relatedArticle in relatedArticlesContent:
-        similarityScore = calcSemanticVariationBetweenTwo(
-            rareWordsMainArticle, relatedArticle)
-        relatedArticlesAndSimilarityScores.append(
-            [relatedArticle.url, similarityScore])
+    else:
+        # Find the rare words in the main article here, so we only have to find them once
+        rareWordsMainArticle = extractRareWords(mainArticleContent)
 
-    # sort the list by decreasing similarity score
-    relatedArticlesAndSimilarityScores.sort(key=lambda x: x[1], reverse=False)
+        relatedArticlesAndSimilarityScores = []
+        for relatedArticle in relatedArticlesContent:
+            similarityScore = calcSemanticVariationBetweenTwo(
+                rareWordsMainArticle, relatedArticle)
+            relatedArticlesAndSimilarityScores.append(
+                [relatedArticle.url, similarityScore])
+
+        # sort the list by decreasing similarity score
+        relatedArticlesAndSimilarityScores.sort(key=lambda x: x[1],
+                                                reverse=False)
 
     return relatedArticlesAndSimilarityScores
